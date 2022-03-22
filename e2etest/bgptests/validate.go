fedepaol/metallb

E2e: use the new crd structure
We change the tests to reflect the new structure, and the config updater
to write directly the CRs instead of jumping in the intermediate
configuration.

Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

e2etest/bgptests/validate.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"go.universe.tf/metallb/e2etest/pkg/config"
+	metallbv1beta2 "go.universe.tf/metallb/api/v1beta2"
 	"go.universe.tf/metallb/e2etest/pkg/executor"
 	"go.universe.tf/metallb/e2etest/pkg/frr"
 	frrcontainer "go.universe.tf/metallb/e2etest/pkg/frr/container"
@@ -118,18 +118,18 @@ func frrIsPairedOnPods(cs clientset.Interface, n *frrcontainer.FRR, ipFamily ipf
 	}, 4*time.Minute, 1*time.Second).Should(BeNil())
 }
 
-func checkBFDConfigPropagated(nodeConfig config.BfdProfile, peerConfig bgpfrr.BFDPeer) error {
+func checkBFDConfigPropagated(nodeConfig metallbv1beta2.BFDProfile, peerConfig bgpfrr.BFDPeer) error {
 	if peerConfig.Status != "up" {
 		return fmt.Errorf("Peer status not up")
 	}
-	if peerConfig.RemoteReceiveInterval != int(*nodeConfig.ReceiveInterval) {
-		return fmt.Errorf("RemoteReceiveInterval: expecting %d, got %d", *nodeConfig.ReceiveInterval, peerConfig.RemoteReceiveInterval)
+	if peerConfig.RemoteReceiveInterval != int(*nodeConfig.Spec.ReceiveInterval) {
+		return fmt.Errorf("RemoteReceiveInterval: expecting %d, got %d", *nodeConfig.Spec.ReceiveInterval, peerConfig.RemoteReceiveInterval)
 	}
-	if peerConfig.RemoteTransmitInterval != int(*nodeConfig.TransmitInterval) {
-		return fmt.Errorf("RemoteTransmitInterval: expecting %d, got %d", *nodeConfig.TransmitInterval, peerConfig.RemoteTransmitInterval)
+	if peerConfig.RemoteTransmitInterval != int(*nodeConfig.Spec.TransmitInterval) {
+		return fmt.Errorf("RemoteTransmitInterval: expecting %d, got %d", *nodeConfig.Spec.TransmitInterval, peerConfig.RemoteTransmitInterval)
 	}
-	if peerConfig.RemoteEchoInterval != int(*nodeConfig.EchoInterval) {
-		return fmt.Errorf("EchoInterval: expecting %d, got %d", *nodeConfig.EchoInterval, peerConfig.RemoteEchoInterval)
+	if peerConfig.RemoteEchoInterval != int(*nodeConfig.Spec.EchoInterval) {
+		return fmt.Errorf("EchoInterval: expecting %d, got %d", *nodeConfig.Spec.EchoInterval, peerConfig.RemoteEchoInterval)
 	}
 	return nil
 }

e2etest/pkg/config/parse.go
@@ -2,90 +2,22 @@
 
 package config
 
-// Proto holds the protocol we are speaking.
-type Proto string
+import metallbv1beta2 "go.universe.tf/metallb/api/v1beta2"
 
-// MetalLB supported protocols.
-const (
-	BGP    Proto = "bgp"
-	Layer2 Proto = "layer2"
-)
-
-// configFile is the configuration as parsed out of the ConfigMap,
-// without validation or useful high level types.
-type File struct {
-	Peers          []Peer            `yaml:"peers,omitempty"`
-	BGPCommunities map[string]string `yaml:"bgp-communities,omitempty"`
-	Pools          []AddressPool     `yaml:"address-pools,omitempty"`
-	BFDProfiles    []BfdProfile      `yaml:"bfd-profiles"`
-}
-
-type Peer struct {
-	MyASN         uint32         `yaml:"my-asn,omitempty"`
-	ASN           uint32         `yaml:"peer-asn,omitempty"`
-	Addr          string         `yaml:"peer-address,omitempty"`
-	SrcAddr       string         `yaml:"source-address,omitempty"`
-	Port          uint16         `yaml:"peer-port,omitempty"`
-	HoldTime      string         `yaml:"hold-time,omitempty"`
-	KeepaliveTime string         `yaml:"keepalive-time,omitempty"`
-	NodeSelectors []NodeSelector `yaml:"node-selectors,omitempty"`
-	Password      string         `yaml:"password,omitempty"`
-	BFDProfile    string         `yaml:"bfd-profile,omitempty"`
-	EBGPMultiHop  bool           `yaml:"ebgp-multihop,omitempty"`
-	RouterID      string         `yaml:"router-id,omitempty"`
-}
-
-type NodeSelector struct {
-	MatchLabels      map[string]string      `yaml:"match-labels,omitempty"`
-	MatchExpressions []SelectorRequirements `yaml:"match-expressions,omitempty"`
-}
-
-type SelectorRequirements struct {
-	Key      string   `yaml:"key,omitempty"`
-	Operator string   `yaml:"operator,omitempty"`
-	Values   []string `yaml:"values,omitempty"`
-}
-
-type AddressPool struct {
-	Protocol          Proto              `yaml:"protocol,omitempty"`
-	Name              string             `yaml:"name,omitempty"`
-	Addresses         []string           `yaml:"addresses,omitempty"`
-	AvoidBuggyIPs     bool               `yaml:"avoid-buggy-ips,omitempty"`
-	AutoAssign        *bool              `yaml:"auto-assign,omitempty"`
-	BGPAdvertisements []BgpAdvertisement `yaml:"bgp-advertisements,omitempty"`
-}
-
-type BgpAdvertisement struct {
-	AggregationLength *int     `yaml:"aggregation-length,omitempty"`
-	LocalPref         *uint32  `yaml:"localpref,omitempty"`
-	Communities       []string `yaml:"communities,omitempty"`
-}
-
-type BfdProfile struct {
-	Name             string  `yaml:"name"`
-	ReceiveInterval  *uint32 `yaml:"receive-interval,omitempty"`
-	TransmitInterval *uint32 `yaml:"transmit-interval,omitempty"`
-	DetectMultiplier *uint32 `yaml:"detect-multiplier,omitempty"`
-	EchoInterval     *uint32 `yaml:"echo-interval,omitempty"`
-	EchoMode         *bool   `yaml:"echo-mode,omitempty"`
-	PassiveMode      *bool   `yaml:"passive-mode,omitempty"`
-	MinimumTTL       *uint32 `yaml:"minimum-ttl,omitempty"`
-}
-
-func BFDProfileWithDefaults(profile BfdProfile, multiHop bool) BfdProfile {
-	res := BfdProfile{}
+func BFDProfileWithDefaults(profile metallbv1beta2.BFDProfile, multiHop bool) metallbv1beta2.BFDProfile {
+	res := metallbv1beta2.BFDProfile{}
 	res.Name = profile.Name
-	res.ReceiveInterval = valueWithDefault(profile.ReceiveInterval, 300)
-	res.TransmitInterval = valueWithDefault(profile.TransmitInterval, 300)
-	res.DetectMultiplier = valueWithDefault(profile.DetectMultiplier, 3)
-	res.EchoInterval = valueWithDefault(profile.EchoInterval, 50)
-	res.MinimumTTL = valueWithDefault(profile.MinimumTTL, 254)
-	res.EchoMode = profile.EchoMode
-	res.PassiveMode = profile.PassiveMode
+	res.Spec.ReceiveInterval = valueWithDefault(profile.Spec.ReceiveInterval, 300)
+	res.Spec.TransmitInterval = valueWithDefault(profile.Spec.TransmitInterval, 300)
+	res.Spec.DetectMultiplier = valueWithDefault(profile.Spec.DetectMultiplier, 3)
+	res.Spec.EchoInterval = valueWithDefault(profile.Spec.EchoInterval, 50)
+	res.Spec.MinimumTTL = valueWithDefault(profile.Spec.MinimumTTL, 254)
+	res.Spec.EchoMode = profile.Spec.EchoMode
+	res.Spec.PassiveMode = profile.Spec.PassiveMode
 
 	if multiHop {
-		res.EchoMode = boolPtr(false)
-		res.EchoInterval = uint32Ptr(50)
+		res.Spec.EchoMode = boolPtr(false)
+		res.Spec.EchoInterval = uint32Ptr(50)
 	}
 
 	return res

e2etest/pkg/config/validate.go
@@ -9,14 +9,15 @@ import (
 
 	"github.com/mikioh/ipaddr"
 	"github.com/pkg/errors"
+	metallbv1beta2 "go.universe.tf/metallb/api/v1beta2"
 	internalconfig "go.universe.tf/metallb/internal/config"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
-func ValidateIPInRange(addressPools []AddressPool, ip string) error {
+func ValidateIPInRange(addressPools []metallbv1beta2.AddressPool, ip string) error {
 	input := net.ParseIP(ip)
 	for _, addressPool := range addressPools {
-		for _, address := range addressPool.Addresses {
+		for _, address := range addressPool.Spec.Addresses {
 			cidrs, err := internalconfig.ParseCIDR(address)
 			framework.ExpectNoError(err)
 			for _, cidr := range cidrs {
@@ -52,9 +53,9 @@ func GetIPFromRangeByIndex(ipRange string, index int) (string, error) {
 }
 
 // PoolCount returns the number of addresses in a given Pool.
-func PoolCount(p AddressPool) (int64, error) {
+func PoolCount(p metallbv1beta2.AddressPool) (int64, error) {
 	var total int64
-	for _, r := range p.Addresses {
+	for _, r := range p.Spec.Addresses {
 		cidrs, err := internalconfig.ParseCIDR(r)
 		if err != nil {
 			return 0, err
@@ -72,7 +73,7 @@ func PoolCount(p AddressPool) (int64, error) {
 			firstIP := cur.First().IP
 			lastIP := cur.Last().IP
 
-			if p.AvoidBuggyIPs {
+			if p.Spec.AvoidBuggyIPs {
 				if o <= 24 {
 					// A pair of buggy IPs occur for each /24 present in the range.
 					buggies := int64(math.Pow(2, float64(24-o))) * 2

novad03/k8s-meta

e2etest/bgptests/validate.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"go.universe.tf/metallb/e2etest/pkg/config"
+	metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
 	"go.universe.tf/metallb/e2etest/pkg/executor"
 	"go.universe.tf/metallb/e2etest/pkg/frr"
 	frrcontainer "go.universe.tf/metallb/e2etest/pkg/frr/container"
@@ -121,18 +121,18 @@ func frrIsPairedOnPods(cs clientset.Interface, n *frrcontainer.FRR, ipFamily ipf
 	}, 4*time.Minute, 1*time.Second).Should(BeNil())
 }
 
-func checkBFDConfigPropagated(nodeConfig config.BfdProfile, peerConfig bgpfrr.BFDPeer) error {
+func checkBFDConfigPropagated(nodeConfig metallbv1beta1.BFDProfile, peerConfig bgpfrr.BFDPeer) error {
 	if peerConfig.Status != "up" {
 		return fmt.Errorf("Peer status not up")
 	}
-	if peerConfig.RemoteReceiveInterval != int(*nodeConfig.ReceiveInterval) {
-		return fmt.Errorf("RemoteReceiveInterval: expecting %d, got %d", *nodeConfig.ReceiveInterval, peerConfig.RemoteReceiveInterval)
+	if peerConfig.RemoteReceiveInterval != int(*nodeConfig.Spec.ReceiveInterval) {
+		return fmt.Errorf("RemoteReceiveInterval: expecting %d, got %d", *nodeConfig.Spec.ReceiveInterval, peerConfig.RemoteReceiveInterval)
 	}
-	if peerConfig.RemoteTransmitInterval != int(*nodeConfig.TransmitInterval) {
-		return fmt.Errorf("RemoteTransmitInterval: expecting %d, got %d", *nodeConfig.TransmitInterval, peerConfig.RemoteTransmitInterval)
+	if peerConfig.RemoteTransmitInterval != int(*nodeConfig.Spec.TransmitInterval) {
+		return fmt.Errorf("RemoteTransmitInterval: expecting %d, got %d", *nodeConfig.Spec.TransmitInterval, peerConfig.RemoteTransmitInterval)
 	}
-	if peerConfig.RemoteEchoInterval != int(*nodeConfig.EchoInterval) {
-		return fmt.Errorf("EchoInterval: expecting %d, got %d", *nodeConfig.EchoInterval, peerConfig.RemoteEchoInterval)
+	if peerConfig.RemoteEchoInterval != int(*nodeConfig.Spec.EchoInterval) {
+		return fmt.Errorf("EchoInterval: expecting %d, got %d", *nodeConfig.Spec.EchoInterval, peerConfig.RemoteEchoInterval)
 	}
 	return nil
 }

e2etest/pkg/config/parse.go
@@ -2,90 +2,22 @@
 
 package config
 
-// Proto holds the protocol we are speaking.
-type Proto string
+import metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
 
-// MetalLB supported protocols.
-const (
-	BGP    Proto = "bgp"
-	Layer2 Proto = "layer2"
-)
-
-// configFile is the configuration as parsed out of the ConfigMap,
-// without validation or useful high level types.
-type File struct {
-	Peers          []Peer            `yaml:"peers,omitempty"`
-	BGPCommunities map[string]string `yaml:"bgp-communities,omitempty"`
-	Pools          []AddressPool     `yaml:"address-pools,omitempty"`
-	BFDProfiles    []BfdProfile      `yaml:"bfd-profiles"`
-}
-
-type Peer struct {
-	MyASN         uint32         `yaml:"my-asn,omitempty"`
-	ASN           uint32         `yaml:"peer-asn,omitempty"`
-	Addr          string         `yaml:"peer-address,omitempty"`
-	SrcAddr       string         `yaml:"source-address,omitempty"`
-	Port          uint16         `yaml:"peer-port,omitempty"`
-	HoldTime      string         `yaml:"hold-time,omitempty"`
-	KeepaliveTime string         `yaml:"keepalive-time,omitempty"`
-	NodeSelectors []NodeSelector `yaml:"node-selectors,omitempty"`
-	Password      string         `yaml:"password,omitempty"`
-	BFDProfile    string         `yaml:"bfd-profile,omitempty"`
-	EBGPMultiHop  bool           `yaml:"ebgp-multihop,omitempty"`
-	RouterID      string         `yaml:"router-id,omitempty"`
-}
-
-type NodeSelector struct {
-	MatchLabels      map[string]string      `yaml:"match-labels,omitempty"`
-	MatchExpressions []SelectorRequirements `yaml:"match-expressions,omitempty"`
-}
-
-type SelectorRequirements struct {
-	Key      string   `yaml:"key,omitempty"`
-	Operator string   `yaml:"operator,omitempty"`
-	Values   []string `yaml:"values,omitempty"`
-}
-
-type AddressPool struct {
-	Protocol          Proto              `yaml:"protocol,omitempty"`
-	Name              string             `yaml:"name,omitempty"`
-	Addresses         []string           `yaml:"addresses,omitempty"`
-	AvoidBuggyIPs     bool               `yaml:"avoid-buggy-ips,omitempty"`
-	AutoAssign        *bool              `yaml:"auto-assign,omitempty"`
-	BGPAdvertisements []BgpAdvertisement `yaml:"bgp-advertisements,omitempty"`
-}
-
-type BgpAdvertisement struct {
-	AggregationLength *int     `yaml:"aggregation-length,omitempty"`
-	LocalPref         *uint32  `yaml:"localpref,omitempty"`
-	Communities       []string `yaml:"communities,omitempty"`
-}
-
-type BfdProfile struct {
-	Name             string  `yaml:"name"`
-	ReceiveInterval  *uint32 `yaml:"receive-interval,omitempty"`
-	TransmitInterval *uint32 `yaml:"transmit-interval,omitempty"`
-	DetectMultiplier *uint32 `yaml:"detect-multiplier,omitempty"`
-	EchoInterval     *uint32 `yaml:"echo-interval,omitempty"`
-	EchoMode         *bool   `yaml:"echo-mode,omitempty"`
-	PassiveMode      *bool   `yaml:"passive-mode,omitempty"`
-	MinimumTTL       *uint32 `yaml:"minimum-ttl,omitempty"`
-}
-
-func BFDProfileWithDefaults(profile BfdProfile, multiHop bool) BfdProfile {
-	res := BfdProfile{}
+func BFDProfileWithDefaults(profile metallbv1beta1.BFDProfile, multiHop bool) metallbv1beta1.BFDProfile {
+	res := metallbv1beta1.BFDProfile{}
 	res.Name = profile.Name
-	res.ReceiveInterval = valueWithDefault(profile.ReceiveInterval, 300)
-	res.TransmitInterval = valueWithDefault(profile.TransmitInterval, 300)
-	res.DetectMultiplier = valueWithDefault(profile.DetectMultiplier, 3)
-	res.EchoInterval = valueWithDefault(profile.EchoInterval, 50)
-	res.MinimumTTL = valueWithDefault(profile.MinimumTTL, 254)
-	res.EchoMode = profile.EchoMode
-	res.PassiveMode = profile.PassiveMode
+	res.Spec.ReceiveInterval = valueWithDefault(profile.Spec.ReceiveInterval, 300)
+	res.Spec.TransmitInterval = valueWithDefault(profile.Spec.TransmitInterval, 300)
+	res.Spec.DetectMultiplier = valueWithDefault(profile.Spec.DetectMultiplier, 3)
+	res.Spec.EchoInterval = valueWithDefault(profile.Spec.EchoInterval, 50)
+	res.Spec.MinimumTTL = valueWithDefault(profile.Spec.MinimumTTL, 254)
+	res.Spec.EchoMode = profile.Spec.EchoMode
+	res.Spec.PassiveMode = profile.Spec.PassiveMode
 
 	if multiHop {
-		res.EchoMode = boolPtr(false)
-		res.EchoInterval = uint32Ptr(50)
+		res.Spec.EchoMode = boolPtr(false)
+		res.Spec.EchoInterval = uint32Ptr(50)
 	}
 
 	return res

e2etest/pkg/config/validate.go
@@ -9,14 +9,15 @@ import (
 
 	"github.com/mikioh/ipaddr"
 	"github.com/pkg/errors"
+	metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
 	internalconfig "go.universe.tf/metallb/internal/config"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
-func ValidateIPInRange(addressPools []AddressPool, ip string) error {
+func ValidateIPInRange(addressPools []metallbv1beta1.IPPool, ip string) error {
 	input := net.ParseIP(ip)
 	for _, addressPool := range addressPools {
-		for _, address := range addressPool.Addresses {
+		for _, address := range addressPool.Spec.Addresses {
 			cidrs, err := internalconfig.ParseCIDR(address)
 			framework.ExpectNoError(err)
 			for _, cidr := range cidrs {
@@ -52,9 +53,9 @@ func GetIPFromRangeByIndex(ipRange string, index int) (string, error) {
 }
 
 // PoolCount returns the number of addresses in a given Pool.
-func PoolCount(p AddressPool) (int64, error) {
+func PoolCount(p metallbv1beta1.IPPool) (int64, error) {
 	var total int64
-	for _, r := range p.Addresses {
+	for _, r := range p.Spec.Addresses {
 		cidrs, err := internalconfig.ParseCIDR(r)
 		if err != nil {
 			return 0, err
@@ -72,7 +73,7 @@ func PoolCount(p AddressPool) (int64, error) {
 			firstIP := cur.First().IP
 			lastIP := cur.Last().IP
 
-			if p.AvoidBuggyIPs {
+			if p.Spec.AvoidBuggyIPs {
 				if o <= 24 {
 					// A pair of buggy IPs occur for each /24 present in the range.
 					buggies := int64(math.Pow(2, float64(24-o))) * 2