fedepaol/metallb

Configuration: use the new CRD layout
With the separation of the addresspool from the protocol and the
introduction of l2 and bgp advertisement, we change the internal
structure so that the pool also contains the reference to its
advertisements.

Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

internal/config/validate_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"go.universe.tf/metallb/api/v1beta1"
+	"go.universe.tf/metallb/api/v1beta2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -19,9 +19,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "peer with bfd profile",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:    "1.2.3.4",
 							BFDProfile: "foo",
 						},
@@ -33,14 +33,14 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "bfd profile set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
 				},
-				BFDProfiles: []v1beta1.BFDProfile{
+				BFDProfiles: []v1beta2.BFDProfile{
 					{
 						ObjectMeta: v1.ObjectMeta{Name: "foo"},
 					},
@@ -51,11 +51,17 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "v6 address",
 			config: ClusterResources{
-				Pools: []v1beta1.AddressPool{
+				Pools: []v1beta2.AddressPool{
 					{
-						Spec: v1beta1.AddressPoolSpec{
-							Protocol:  "bgp",
+						Spec: v1beta2.AddressPoolSpec{
 							Addresses: []string{"2001:db8::/64"},
+						},
+					},
+				},
+				BGPAdvs: []v1beta2.BGPAdvertisement{
+					{
+						ObjectMeta: v1.ObjectMeta{
+							Name: "foo",
 						},
 					},
 				},
@@ -65,9 +71,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "keepalive time",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							KeepaliveTime: v1.Duration{Duration: time.Second},
 						},
 					},
@@ -78,9 +84,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "should pass",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
@@ -111,9 +117,9 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "peer with routerid",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
@@ -124,21 +130,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "routerid set, one different",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.5",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.6",
 							RouterID: "1.2.3.5",
 						},
@@ -150,21 +156,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "routerid set, one not set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.5",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 						},
 					},
@@ -175,15 +181,15 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "bfd profile set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
 					},
 				},
-				BFDProfiles: []v1beta1.BFDProfile{
+				BFDProfiles: []v1beta2.BFDProfile{
 					{
 						ObjectMeta: v1.ObjectMeta{
 							Name: "foo",
@@ -195,21 +201,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "myAsn set, all equals",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.5",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 							MyASN:   123,
 						},
@@ -220,21 +226,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "myAsn set, one different",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.5",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 							MyASN:   124,
 						},
@@ -246,14 +252,14 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "duplicate bgp address",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},

internal/k8s/controllers/config_controller.go
@@ -18,12 +18,11 @@ package controllers
 
 import (
 	"context"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
+	metallbv1beta2 "go.universe.tf/metallb/api/v1beta2"
 	"go.universe.tf/metallb/internal/config"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -34,8 +33,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
-
-const RetryPeriod = 10 * time.Second
 
 type ConfigReconciler struct {
 	client.Client
@@ -48,28 +45,41 @@ type ConfigReconciler struct {
 }
 
 //+kubebuilder:rbac:groups=metallb.io,resources=bgppeers,verbs=get;list;watch;
-//+kubebuilder:rbac:groups=metallb.io,resources=addresspools,verbs=get;list;watch;create;
-//+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;create;
+//+kubebuilder:rbac:groups=metallb.io,resources=addresspools,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=bgpadvertisement,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=l2advertisement,verbs=get;list;watch;
 
 func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	level.Info(r.Logger).Log("controller", "ConfigReconciler", "start reconcile", req.NamespacedName.String())
 	defer level.Info(r.Logger).Log("controller", "ConfigReconciler", "end reconcile", req.NamespacedName.String())
 
-	var addressPools metallbv1beta1.AddressPoolList
+	var addressPools metallbv1beta2.AddressPoolList
 	if err := r.List(ctx, &addressPools, client.InNamespace(r.Namespace)); err != nil {
 		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get addresspools", "error", err)
 		return ctrl.Result{}, err
 	}
-
-	var bgpPeers metallbv1beta1.BGPPeerList
+	var bgpPeers metallbv1beta2.BGPPeerList
 	if err := r.List(ctx, &bgpPeers, client.InNamespace(r.Namespace)); err != nil {
 		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get bgppeers", "error", err)
 		return ctrl.Result{}, err
 	}
 
-	var bfdProfiles metallbv1beta1.BFDProfileList
+	var bfdProfiles metallbv1beta2.BFDProfileList
 	if err := r.List(ctx, &bfdProfiles, client.InNamespace(r.Namespace)); err != nil {
 		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get bfdprofiles", "error", err)
+		return ctrl.Result{}, err
+	}
+
+	var l2Advertisements metallbv1beta2.L2AdvertisementList
+	if err := r.List(ctx, &l2Advertisements, client.InNamespace(r.Namespace)); err != nil {
+		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get l2 advertisements", "error", err)
+		return ctrl.Result{}, err
+	}
+
+	var bgpAdvertisements metallbv1beta2.BGPAdvertisementList
+	if err := r.List(ctx, &bgpAdvertisements, client.InNamespace(r.Namespace)); err != nil {
+		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get bgp advertisements", "error", err)
 		return ctrl.Result{}, err
 	}
 
@@ -77,6 +87,8 @@ func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		Pools:       addressPools.Items,
 		Peers:       bgpPeers.Items,
 		BFDProfiles: bfdProfiles.Items,
+		L2Advs:      l2Advertisements.Items,
+		BGPAdvs:     bgpAdvertisements.Items,
 	}
 
 	level.Debug(r.Logger).Log("controller", "ConfigReconciler", "metallb CRs", spew.Sdump(metallbCRs))
@@ -125,8 +137,10 @@ func (r *ConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		},
 	}
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&metallbv1beta1.BGPPeer{}, builder.WithPredicates(p)).
-		Watches(&source.Kind{Type: &metallbv1beta1.AddressPool{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
-		Watches(&source.Kind{Type: &metallbv1beta1.BFDProfile{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		For(&metallbv1beta2.BGPPeer{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta2.AddressPool{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta2.BGPAdvertisement{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta2.L2Advertisement{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta2.BFDProfile{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
 		Complete(r)
 }

novad03/k8s-meta

internal/config/validate_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"go.universe.tf/metallb/api/v1beta1"
+	"go.universe.tf/metallb/api/v1beta2"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -19,9 +20,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "peer with bfd profile",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:    "1.2.3.4",
 							BFDProfile: "foo",
 						},
@@ -33,9 +34,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "bfd profile set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
@@ -51,11 +52,17 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "v6 address",
 			config: ClusterResources{
-				Pools: []v1beta1.AddressPool{
+				Pools: []v1beta1.IPPool{
 					{
-						Spec: v1beta1.AddressPoolSpec{
-							Protocol:  "bgp",
+						Spec: v1beta1.IPPoolSpec{
 							Addresses: []string{"2001:db8::/64"},
+						},
+					},
+				},
+				BGPAdvs: []v1beta1.BGPAdvertisement{
+					{
+						ObjectMeta: v1.ObjectMeta{
+							Name: "foo",
 						},
 					},
 				},
@@ -65,9 +72,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "keepalive time",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							KeepaliveTime: v1.Duration{Duration: time.Second},
 						},
 					},
@@ -78,9 +85,9 @@ func TestValidate(t *testing.T) {
 		{
 			desc: "should pass",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
@@ -111,9 +118,9 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "peer with routerid",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
@@ -124,21 +131,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "routerid set, one different",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.5",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.6",
 							RouterID: "1.2.3.5",
 						},
@@ -150,21 +157,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "routerid set, one not set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.5",
 							RouterID: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 						},
 					},
@@ -175,9 +182,9 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "bfd profile set",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address:  "1.2.3.4",
 							RouterID: "1.2.3.4",
 						},
@@ -195,21 +202,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "myAsn set, all equals",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.5",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 							MyASN:   123,
 						},
@@ -220,21 +227,21 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "myAsn set, one different",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.5",
 							MyASN:   123,
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.6",
 							MyASN:   124,
 						},
@@ -246,14 +253,14 @@ func TestValidateFRR(t *testing.T) {
 		{
 			desc: "duplicate bgp address",
 			config: ClusterResources{
-				Peers: []v1beta1.BGPPeer{
+				Peers: []v1beta2.BGPPeer{
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},
 					{
-						Spec: v1beta1.BGPPeerSpec{
+						Spec: v1beta2.BGPPeerSpec{
 							Address: "1.2.3.4",
 						},
 					},

internal/k8s/controllers/config_controller.go
@@ -18,12 +18,12 @@ package controllers
 
 import (
 	"context"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	metallbv1beta1 "go.universe.tf/metallb/api/v1beta1"
+	metallbv1beta2 "go.universe.tf/metallb/api/v1beta2"
 	"go.universe.tf/metallb/internal/config"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -34,8 +34,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
-
-const RetryPeriod = 10 * time.Second
 
 type ConfigReconciler struct {
 	client.Client
@@ -48,8 +46,10 @@ type ConfigReconciler struct {
 }
 
 //+kubebuilder:rbac:groups=metallb.io,resources=bgppeers,verbs=get;list;watch;
-//+kubebuilder:rbac:groups=metallb.io,resources=addresspools,verbs=get;list;watch;create;
-//+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;create;
+//+kubebuilder:rbac:groups=metallb.io,resources=addresspools,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=bfdprofiles,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=bgpadvertisement,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=metallb.io,resources=l2advertisement,verbs=get;list;watch;
 
 func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	level.Info(r.Logger).Log("controller", "ConfigReconciler", "start reconcile", req.NamespacedName.String())
@@ -61,7 +61,13 @@ func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, err
 	}
 
-	var bgpPeers metallbv1beta1.BGPPeerList
+	var ipPools metallbv1beta1.IPPoolList
+	if err := r.List(ctx, &ipPools, client.InNamespace(r.Namespace)); err != nil {
+		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get addresspools", "error", err)
+		return ctrl.Result{}, err
+	}
+
+	var bgpPeers metallbv1beta2.BGPPeerList
 	if err := r.List(ctx, &bgpPeers, client.InNamespace(r.Namespace)); err != nil {
 		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get bgppeers", "error", err)
 		return ctrl.Result{}, err
@@ -73,10 +79,24 @@ func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, err
 	}
 
+	var l2Advertisements metallbv1beta1.L2AdvertisementList
+	if err := r.List(ctx, &l2Advertisements, client.InNamespace(r.Namespace)); err != nil {
+		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get l2 advertisements", "error", err)
+		return ctrl.Result{}, err
+	}
+
+	var bgpAdvertisements metallbv1beta1.BGPAdvertisementList
+	if err := r.List(ctx, &bgpAdvertisements, client.InNamespace(r.Namespace)); err != nil {
+		level.Error(r.Logger).Log("controller", "ConfigReconciler", "error", "failed to get bgp advertisements", "error", err)
+		return ctrl.Result{}, err
+	}
+
 	metallbCRs := config.ClusterResources{
-		Pools:       addressPools.Items,
+		Pools:       ipPools.Items,
 		Peers:       bgpPeers.Items,
 		BFDProfiles: bfdProfiles.Items,
+		L2Advs:      l2Advertisements.Items,
+		BGPAdvs:     bgpAdvertisements.Items,
 	}
 
 	level.Debug(r.Logger).Log("controller", "ConfigReconciler", "metallb CRs", spew.Sdump(metallbCRs))
@@ -125,8 +145,10 @@ func (r *ConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		},
 	}
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&metallbv1beta1.BGPPeer{}, builder.WithPredicates(p)).
-		Watches(&source.Kind{Type: &metallbv1beta1.AddressPool{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		For(&metallbv1beta2.BGPPeer{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta1.IPPool{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta1.BGPAdvertisement{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
+		Watches(&source.Kind{Type: &metallbv1beta1.L2Advertisement{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
 		Watches(&source.Kind{Type: &metallbv1beta1.BFDProfile{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(p)).
 		Complete(r)
 }