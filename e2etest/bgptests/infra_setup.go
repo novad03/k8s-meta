metallb/metallb

E2E: setup the infra to add nodes to a network via VRF
In order to verify that MetalLB announcement via VRF works, we create a
new docker network and provide the infrastructure to put the
corresponding interface inside a linux VRF.

Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

e2etest/bgptests/infra_setup.go
@@ -19,6 +19,8 @@ import (
 
 const (
 	multiHopNetwork      = "multi-hop-net"
+	vrfNetwork           = "vrf-net"
+	vrfName              = "red"
 	metalLBASN           = 64512
 	externalASN          = 4200000000
 	nextHopContainerName = "ebgp-single-hop"
@@ -30,6 +32,7 @@ var (
 	hostIPv6          string
 	multiHopRoutes    map[string]container.NetworkSettings
 	FRRContainers     []*frrcontainer.FRR
+	VRFFRRContainers  []*frrcontainer.FRR
 )
 
 func init() {
@@ -49,7 +52,7 @@ func init() {
 
 // InfraSetup brings up the external container mimicking external routers, and set up the routing needed for
 // testing.
-func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string, cs *clientset.Clientset) ([]*frrcontainer.FRR, error) {
+func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string, cs *clientset.Clientset) ([]*frrcontainer.FRR, []*frrcontainer.FRR, error) {
 	/*
 		We have 2 ways in which we setup the containers for the tests:
 		1 - The user requested the containers to use the 'host' network
@@ -130,65 +133,90 @@ func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string
 		},
 	}
 
-	var res []*frrcontainer.FRR
-	var err error
+	ebgpSingleHopContainerVRFConfig := frrcontainer.Config{
+		Name:    "ebgp-vrf-single-hop",
+		Network: vrfNetwork,
+		Neighbor: frrconfig.NeighborConfig{
+			ASN:      metalLBASN,
+			MultiHop: false,
+		},
+		Router: frrconfig.RouterConfig{
+			ASN:     externalASN,
+			BGPPort: 179,
+			VRF:     "red",
+		},
+	}
+
 	if externalContainers != "" {
 		err := validateContainersNames(externalContainers)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		configs, err := configsFor(externalContainers, ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
-			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
+			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig, ebgpSingleHopContainerVRFConfig)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
-		res, err = frrcontainer.ConfigureExisting(configs...)
+		res, err := frrcontainer.ConfigureExisting(configs...)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		if containsMultiHop(res) {
 			err = multiHopSetUp(res, cs)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 		}
-	} else if containersNetwork == "host" {
-		res, err = frrcontainer.Create(ibgpSingleHopContainerConfig)
+		return res, []*frrcontainer.FRR{}, nil
+	}
+	if containersNetwork == "host" {
+		res, err := frrcontainer.Create(ibgpSingleHopContainerConfig)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
-	} else {
-		Expect(len(ipv4Addresses)).Should(BeNumerically(">=", 2))
-		Expect(len(ipv6Addresses)).Should(BeNumerically(">=", 2))
-
-		ibgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[0]
-		ibgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[0]
-		ebgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[1]
-		ebgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[1]
-
-		var out string
-		out, err = executor.Host.Exec(executor.ContainerRuntime, "network", "create", multiHopNetwork, "--ipv6",
-			"--driver=bridge", "--subnet=172.30.0.0/16", "--subnet=fc00:f853:ccd:e798::/64")
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to create %s: %s", multiHopNetwork, out)
-		}
-
-		res, err = frrcontainer.Create(ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
-			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
-		if err != nil {
-			return nil, err
-		}
-
-		err = multiHopSetUp(res, cs)
-		if err != nil {
-			return nil, err
-		}
+		return res, []*frrcontainer.FRR{}, nil
 	}
 
-	return res, nil
+	Expect(len(ipv4Addresses)).Should(BeNumerically(">=", 2))
+	Expect(len(ipv6Addresses)).Should(BeNumerically(">=", 2))
+
+	ibgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[0]
+	ibgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[0]
+	ebgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[1]
+	ebgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[1]
+
+	var out string
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "create", multiHopNetwork, "--ipv6",
+		"--driver=bridge", "--subnet=172.30.0.0/16", "--subnet=fc00:f853:ccd:e798::/64")
+	if err != nil && !strings.Contains(out, "already exists") {
+		return nil, nil, errors.Wrapf(err, "failed to create %s: %s", multiHopNetwork, out)
+	}
+
+	out, err = executor.Host.Exec(executor.ContainerRuntime, "network", "create", vrfNetwork, "--ipv6",
+		"--driver=bridge", "--subnet=172.31.0.0/16", "--subnet=fc00:f853:ccd:e799::/64")
+	if err != nil && !strings.Contains(out, "already exists") {
+		return nil, nil, errors.Wrapf(err, "failed to create %s: %s", vrfNetwork, out)
+	}
+
+	containers, err := frrcontainer.Create(ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
+		ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+	vrfContainers, err := frrcontainer.Create(ebgpSingleHopContainerVRFConfig)
+
+	err = multiHopSetUp(containers, cs)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = vrfSetup(cs)
+	if err != nil {
+		return nil, nil, err
+	}
+	return containers, vrfContainers, nil
 }
 
 // multiHopSetUp connects the ebgp-single-hop container to the multi-hop-net network,
@@ -221,8 +249,27 @@ func multiHopSetUp(containers []*frrcontainer.FRR, cs *clientset.Clientset) erro
 	return nil
 }
 
+func vrfSetup(cs *clientset.Clientset) error {
+	speakerPods, err := metallb.SpeakerPods(cs)
+	if err != nil {
+		return err
+	}
+	for _, pod := range speakerPods {
+		err := addContainerToNetwork(pod.Spec.NodeName, vrfNetwork)
+		if err != nil {
+			return errors.Wrapf(err, "Failed to connect %s to %s", pod.Spec.NodeName, vrfNetwork)
+		}
+
+		err = container.AddNetworkToVRF(pod.Spec.NodeName, vrfNetwork, vrfName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // InfraTearDown tears down the containers and the routes needed for bgp testing.
-func InfraTearDown(containers []*frrcontainer.FRR, cs *clientset.Clientset) error {
+func InfraTearDown(cs *clientset.Clientset, containers []*frrcontainer.FRR) error {
 	err := frrcontainer.Delete(containers)
 	if err != nil {
 		return err
@@ -230,6 +277,11 @@ func InfraTearDown(containers []*frrcontainer.FRR, cs *clientset.Clientset) erro
 
 	err = multiHopTearDown(cs)
 	if err != nil {
+		return err
+	}
+
+	err = vrfTeardown(cs)
+	if err == nil {
 		return err
 	}
 
@@ -260,6 +312,31 @@ func multiHopTearDown(cs *clientset.Clientset) error {
 
 	}
 
+	return nil
+}
+
+func vrfTeardown(cs *clientset.Clientset) error {
+	_, err := executor.Host.Exec(executor.ContainerRuntime, "network", "inspect", vrfNetwork)
+	if err != nil {
+		return nil
+	}
+
+	speakerPods, err := metallb.SpeakerPods(cs)
+	if err != nil {
+		return nil
+	}
+
+	for _, pod := range speakerPods {
+		err := removeContainerFromNetwork(pod.Spec.NodeName, vrfNetwork)
+		if err != nil {
+			return err
+		}
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "rm", vrfNetwork)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to remove %s: %s", multiHopNetwork, out)
+	}
 	return nil
 }
 
@@ -338,4 +415,38 @@ func containsMultiHop(frrContainers []*frrcontainer.FRR) bool {
 	}
 
 	return multiHop
+}
+
+func addContainerToNetwork(containerName, network string) error {
+	networks, err := container.Networks(containerName)
+	if err != nil {
+		return err
+	}
+	if _, ok := networks[network]; ok {
+		return nil
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "connect",
+		network, containerName)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to connect %s to %s: %s", containerName, network, out)
+	}
+	return nil
+}
+
+func removeContainerFromNetwork(containerName, network string) error {
+	networks, err := container.Networks(containerName)
+	if err != nil {
+		return err
+	}
+	if _, ok := networks[network]; !ok {
+		return nil
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "disconnect",
+		network, containerName)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to disconnect %s from %s: %s", containerName, network, out)
+	}
+	return nil
 }

e2etest/pkg/routes/interfaces.go
@@ -1,0 +1,142 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"go.universe.tf/metallb/e2etest/pkg/executor"
+)
+
+type InterfaceAddress struct {
+	Ifname   string `json:"ifname"`
+	AddrInfo []struct {
+		Family    string `json:"family"`
+		Local     string `json:"local"`
+		Prefixlen int    `json:"prefixlen"`
+		Scope     string `json:"scope"`
+	} `json:"addr_info"`
+}
+
+func InterfaceForAddress(exec executor.Executor, ipv4Address, ipv6Address string) (string, error) {
+	jsonAddr, err := exec.Exec("ip", "-j", "a")
+	if err != nil {
+		return "", err
+	}
+	res, err := findInterfaceWithAddresses(jsonAddr, ipv4Address, ipv6Address)
+	if err != nil {
+		return "", err
+	}
+	return res, nil
+}
+
+type InterfaceLink struct {
+	Ifname string `json:"ifname"`
+	Master string `json:"master"`
+}
+
+type InterfaceNotFoundErr struct {
+	interfaceName string
+}
+
+func (e *InterfaceNotFoundErr) Error() string {
+	return fmt.Sprintf("interface %s not found", e.interfaceName)
+}
+
+func InterfaceExists(exec executor.Executor, intf string) error {
+	interfaces, err := ipLink(exec)
+	if err != nil {
+		return err
+	}
+	for _, i := range interfaces {
+		if i.Ifname == intf {
+			return nil
+		}
+	}
+	return &InterfaceNotFoundErr{interfaceName: intf}
+}
+
+func CreateVRF(exec executor.Executor, vrfName string) error {
+	out, err := exec.Exec("ip", "link", "add", vrfName, "type", "vrf", "table", "2")
+	if err != nil {
+		return errors.Wrapf(err, "failed to create vrf %s : %s", vrfName, out)
+	}
+	out, err = exec.Exec("ip", "link", "set", "dev", vrfName, "up")
+	if err != nil {
+		return errors.Wrapf(err, "failed to set vrf %s up : %s", vrfName, out)
+	}
+	return nil
+}
+
+func AddInterfaceToVRF(exec executor.Executor, intf, vrf, ipv6Address string) error {
+	links, err := ipLink(exec)
+	if err != nil {
+		return err
+	}
+	for _, l := range links {
+		if l.Ifname != intf {
+			continue
+		}
+		if l.Master != "" && l.Master != vrf {
+			return fmt.Errorf("interface %s already has a master %s different from %s", intf, l.Master, vrf)
+		}
+		if l.Master == vrf { // Already set
+			return nil
+		}
+	}
+	out, err := exec.Exec("ip", "link", "set", "dev", intf, "master", vrf)
+	if err != nil {
+		return errors.Wrapf(err, "failed to set master %s to %s : %s", vrf, intf, out)
+	}
+	// we need this because moving the interface to the vrf removes the v6 IP
+	out, err = exec.Exec("ip", "-6", "addr", "add", ipv6Address+"/64", "dev", intf)
+	if err != nil {
+		return errors.Wrapf(err, "failed to set master %s to %s : %s", vrf, intf, out)
+	}
+	return nil
+}
+
+func findInterfaceWithAddresses(jsonIPOutput string, ipv4Address, ipv6Address string) (string, error) {
+	var interfaces []InterfaceAddress
+	err := json.Unmarshal([]byte(jsonIPOutput), &interfaces)
+	if err != nil {
+		return "", err
+	}
+
+	foundV4, foundV6 := false, false
+	for _, i := range interfaces {
+		for _, info := range i.AddrInfo {
+			if info.Family == "inet" && info.Local == ipv4Address {
+				foundV4 = true
+			}
+			if info.Family == "inet6" && info.Scope == "global" && info.Local == ipv6Address {
+				foundV6 = true
+			}
+		}
+		if foundV4 && !foundV6 {
+			return "", fmt.Errorf("interface %s has v4 address %s but not v6 address %s", i.Ifname, ipv4Address, ipv6Address)
+		}
+		if !foundV4 && foundV6 {
+			return "", fmt.Errorf("interface %s does not have v4 address %s but has v6 address %s", i.Ifname, ipv4Address, ipv6Address)
+		}
+		if foundV4 && foundV6 {
+			return i.Ifname, nil
+		}
+	}
+	return "", fmt.Errorf("could not find interface on with ipv4 addres %s and ipv6 address %s", ipv4Address, ipv6Address)
+}
+
+func ipLink(exec executor.Executor) ([]InterfaceLink, error) {
+	links, err := exec.Exec("ip", "-j", "l")
+	if err != nil {
+		return nil, err
+	}
+	var interfaces []InterfaceLink
+	err = json.Unmarshal([]byte(links), &interfaces)
+	if err != nil {
+		return nil, err
+	}
+	return interfaces, nil
+}

novad03/k8s-meta

e2etest/bgptests/infra_setup.go
@@ -19,6 +19,8 @@ import (
 
 const (
 	multiHopNetwork      = "multi-hop-net"
+	vrfNetwork           = "vrf-net"
+	vrfName              = "red"
 	metalLBASN           = 64512
 	externalASN          = 4200000000
 	nextHopContainerName = "ebgp-single-hop"
@@ -30,6 +32,7 @@ var (
 	hostIPv6          string
 	multiHopRoutes    map[string]container.NetworkSettings
 	FRRContainers     []*frrcontainer.FRR
+	VRFFRRContainers  []*frrcontainer.FRR
 )
 
 func init() {
@@ -49,7 +52,7 @@ func init() {
 
 // InfraSetup brings up the external container mimicking external routers, and set up the routing needed for
 // testing.
-func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string, cs *clientset.Clientset) ([]*frrcontainer.FRR, error) {
+func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string, cs *clientset.Clientset) ([]*frrcontainer.FRR, []*frrcontainer.FRR, error) {
 	/*
 		We have 2 ways in which we setup the containers for the tests:
 		1 - The user requested the containers to use the 'host' network
@@ -130,65 +133,90 @@ func InfraSetup(ipv4Addresses, ipv6Addresses []string, externalContainers string
 		},
 	}
 
-	var res []*frrcontainer.FRR
-	var err error
+	ebgpSingleHopContainerVRFConfig := frrcontainer.Config{
+		Name:    "ebgp-vrf-single-hop",
+		Network: vrfNetwork,
+		Neighbor: frrconfig.NeighborConfig{
+			ASN:      metalLBASN,
+			MultiHop: false,
+		},
+		Router: frrconfig.RouterConfig{
+			ASN:     externalASN,
+			BGPPort: 179,
+			VRF:     vrfName,
+		},
+	}
+
 	if externalContainers != "" {
 		err := validateContainersNames(externalContainers)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		configs, err := configsFor(externalContainers, ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
-			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
+			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig, ebgpSingleHopContainerVRFConfig)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
-		res, err = frrcontainer.ConfigureExisting(configs...)
+		res, err := frrcontainer.ConfigureExisting(configs...)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		if containsMultiHop(res) {
 			err = multiHopSetUp(res, cs)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 		}
-	} else if containersNetwork == "host" {
-		res, err = frrcontainer.Create(ibgpSingleHopContainerConfig)
+		return res, []*frrcontainer.FRR{}, nil
+	}
+	if containersNetwork == "host" {
+		res, err := frrcontainer.Create(ibgpSingleHopContainerConfig)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
-	} else {
-		Expect(len(ipv4Addresses)).Should(BeNumerically(">=", 2))
-		Expect(len(ipv6Addresses)).Should(BeNumerically(">=", 2))
-
-		ibgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[0]
-		ibgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[0]
-		ebgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[1]
-		ebgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[1]
-
-		var out string
-		out, err = executor.Host.Exec(executor.ContainerRuntime, "network", "create", multiHopNetwork, "--ipv6",
-			"--driver=bridge", "--subnet=172.30.0.0/16", "--subnet=fc00:f853:ccd:e798::/64")
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to create %s: %s", multiHopNetwork, out)
-		}
-
-		res, err = frrcontainer.Create(ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
-			ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
-		if err != nil {
-			return nil, err
-		}
-
-		err = multiHopSetUp(res, cs)
-		if err != nil {
-			return nil, err
-		}
+		return res, []*frrcontainer.FRR{}, nil
 	}
 
-	return res, nil
+	Expect(len(ipv4Addresses)).Should(BeNumerically(">=", 2))
+	Expect(len(ipv6Addresses)).Should(BeNumerically(">=", 2))
+
+	ibgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[0]
+	ibgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[0]
+	ebgpSingleHopContainerConfig.IPv4Address = ipv4Addresses[1]
+	ebgpSingleHopContainerConfig.IPv6Address = ipv6Addresses[1]
+
+	var out string
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "create", multiHopNetwork, "--ipv6",
+		"--driver=bridge", "--subnet=172.30.0.0/16", "--subnet=fc00:f853:ccd:e798::/64")
+	if err != nil && !strings.Contains(out, "already exists") {
+		return nil, nil, errors.Wrapf(err, "failed to create %s: %s", multiHopNetwork, out)
+	}
+
+	out, err = executor.Host.Exec(executor.ContainerRuntime, "network", "create", vrfNetwork, "--ipv6",
+		"--driver=bridge", "--subnet=172.31.0.0/16", "--subnet=fc00:f853:ccd:e799::/64")
+	if err != nil && !strings.Contains(out, "already exists") {
+		return nil, nil, errors.Wrapf(err, "failed to create %s: %s", vrfNetwork, out)
+	}
+
+	containers, err := frrcontainer.Create(ibgpSingleHopContainerConfig, ibgpMultiHopContainerConfig,
+		ebgpMultiHopContainerConfig, ebgpSingleHopContainerConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+	vrfContainers, err := frrcontainer.Create(ebgpSingleHopContainerVRFConfig)
+
+	err = multiHopSetUp(containers, cs)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = vrfSetup(cs)
+	if err != nil {
+		return nil, nil, err
+	}
+	return containers, vrfContainers, nil
 }
 
 // multiHopSetUp connects the ebgp-single-hop container to the multi-hop-net network,
@@ -221,14 +249,38 @@ func multiHopSetUp(containers []*frrcontainer.FRR, cs *clientset.Clientset) erro
 	return nil
 }
 
+func vrfSetup(cs *clientset.Clientset) error {
+	speakerPods, err := metallb.SpeakerPods(cs)
+	if err != nil {
+		return err
+	}
+	for _, pod := range speakerPods {
+		err := addContainerToNetwork(pod.Spec.NodeName, vrfNetwork)
+		if err != nil {
+			return errors.Wrapf(err, "Failed to connect %s to %s", pod.Spec.NodeName, vrfNetwork)
+		}
+
+		err = container.SetupVRFForNetwork(pod.Spec.NodeName, vrfNetwork, vrfName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // InfraTearDown tears down the containers and the routes needed for bgp testing.
-func InfraTearDown(containers []*frrcontainer.FRR, cs *clientset.Clientset) error {
+func InfraTearDown(cs *clientset.Clientset, containers []*frrcontainer.FRR) error {
 	err := frrcontainer.Delete(containers)
 	if err != nil {
 		return err
 	}
 
 	err = multiHopTearDown(cs)
+	if err != nil {
+		return err
+	}
+
+	err = vrfTeardown(cs)
 	if err != nil {
 		return err
 	}
@@ -260,6 +312,31 @@ func multiHopTearDown(cs *clientset.Clientset) error {
 
 	}
 
+	return nil
+}
+
+func vrfTeardown(cs *clientset.Clientset) error {
+	_, err := executor.Host.Exec(executor.ContainerRuntime, "network", "inspect", vrfNetwork)
+	if err != nil {
+		return nil
+	}
+
+	speakerPods, err := metallb.SpeakerPods(cs)
+	if err != nil {
+		return err
+	}
+
+	for _, pod := range speakerPods {
+		err := removeContainerFromNetwork(pod.Spec.NodeName, vrfNetwork)
+		if err != nil {
+			return err
+		}
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "rm", vrfNetwork)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to remove %s: %s", multiHopNetwork, out)
+	}
 	return nil
 }
 
@@ -338,4 +415,38 @@ func containsMultiHop(frrContainers []*frrcontainer.FRR) bool {
 	}
 
 	return multiHop
+}
+
+func addContainerToNetwork(containerName, network string) error {
+	networks, err := container.Networks(containerName)
+	if err != nil {
+		return err
+	}
+	if _, ok := networks[network]; ok {
+		return nil
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "connect",
+		network, containerName)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to connect %s to %s: %s", containerName, network, out)
+	}
+	return nil
+}
+
+func removeContainerFromNetwork(containerName, network string) error {
+	networks, err := container.Networks(containerName)
+	if err != nil {
+		return err
+	}
+	if _, ok := networks[network]; !ok {
+		return nil
+	}
+
+	out, err := executor.Host.Exec(executor.ContainerRuntime, "network", "disconnect",
+		network, containerName)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to disconnect %s from %s: %s", containerName, network, out)
+	}
+	return nil
 }

e2etest/pkg/routes/interfaces.go
@@ -1,0 +1,152 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"go.universe.tf/metallb/e2etest/pkg/executor"
+)
+
+type InterfaceAddress struct {
+	Ifname   string `json:"ifname"`
+	AddrInfo []struct {
+		Family    string `json:"family"`
+		Local     string `json:"local"`
+		Prefixlen int    `json:"prefixlen"`
+		Scope     string `json:"scope"`
+	} `json:"addr_info"`
+}
+
+func InterfaceForAddress(exec executor.Executor, ipv4Address, ipv6Address string) (string, error) {
+	jsonAddr, err := exec.Exec("ip", "-j", "a")
+	if err != nil {
+		return "", err
+	}
+	res, err := findInterfaceWithAddresses(jsonAddr, ipv4Address, ipv6Address)
+	if err != nil {
+		return "", err
+	}
+	return res, nil
+}
+
+type InterfaceLink struct {
+	Ifname string `json:"ifname"`
+	Master string `json:"master"`
+}
+
+type InterfaceNotFoundErr struct {
+	interfaceName string
+}
+
+func (e *InterfaceNotFoundErr) Error() string {
+	return fmt.Sprintf("interface %s not found", e.interfaceName)
+}
+
+func InterfaceExists(exec executor.Executor, intf string) error {
+	interfaces, err := ipLink(exec)
+	if err != nil {
+		return err
+	}
+	for _, i := range interfaces {
+		if i.Ifname == intf {
+			return nil
+		}
+	}
+	return &InterfaceNotFoundErr{interfaceName: intf}
+}
+
+func CreateVRF(exec executor.Executor, vrfName string) error {
+	err := InterfaceExists(exec, vrfName)
+	// The interface is already there, not doing anything
+	if err == nil {
+		return nil
+	}
+	var notFound *InterfaceNotFoundErr
+	if err != nil && !errors.As(err, &notFound) {
+		return err
+	}
+
+	out, err := exec.Exec("ip", "link", "add", vrfName, "type", "vrf", "table", "2")
+	if err != nil {
+		return errors.Wrapf(err, "failed to create vrf %s : %s", vrfName, out)
+	}
+	out, err = exec.Exec("ip", "link", "set", "dev", vrfName, "up")
+	if err != nil {
+		return errors.Wrapf(err, "failed to set vrf %s up : %s", vrfName, out)
+	}
+	return nil
+}
+
+func AddInterfaceToVRF(exec executor.Executor, intf, vrf, ipv6Address string) error {
+	links, err := ipLink(exec)
+	if err != nil {
+		return err
+	}
+	for _, l := range links {
+		if l.Ifname != intf {
+			continue
+		}
+		if l.Master != "" && l.Master != vrf {
+			return fmt.Errorf("interface %s already has a master %s different from %s", intf, l.Master, vrf)
+		}
+		if l.Master == vrf { // Already set
+			return nil
+		}
+	}
+	out, err := exec.Exec("ip", "link", "set", "dev", intf, "master", vrf)
+	if err != nil {
+		return errors.Wrapf(err, "failed to set master %s to %s : %s", vrf, intf, out)
+	}
+	// we need this because moving the interface to the vrf removes the v6 IP
+	out, err = exec.Exec("ip", "-6", "addr", "add", ipv6Address+"/64", "dev", intf)
+	if err != nil {
+		return errors.Wrapf(err, "failed to add %s to %s : %s", ipv6Address, intf, out)
+	}
+	return nil
+}
+
+func findInterfaceWithAddresses(jsonIPOutput string, ipv4Address, ipv6Address string) (string, error) {
+	var interfaces []InterfaceAddress
+	err := json.Unmarshal([]byte(jsonIPOutput), &interfaces)
+	if err != nil {
+		return "", err
+	}
+
+	foundV4, foundV6 := false, false
+	for _, i := range interfaces {
+		for _, info := range i.AddrInfo {
+			if info.Family == "inet" && info.Local == ipv4Address {
+				foundV4 = true
+			}
+			if info.Family == "inet6" && info.Scope == "global" && info.Local == ipv6Address {
+				foundV6 = true
+			}
+		}
+		if foundV4 && !foundV6 {
+			return "", fmt.Errorf("interface %s has v4 address %s but not v6 address %s", i.Ifname, ipv4Address, ipv6Address)
+		}
+		if !foundV4 && foundV6 {
+			return "", fmt.Errorf("interface %s does not have v4 address %s but has v6 address %s", i.Ifname, ipv4Address, ipv6Address)
+		}
+		if foundV4 && foundV6 {
+			return i.Ifname, nil
+		}
+	}
+	return "", fmt.Errorf("could not find interface on with ipv4 address %s and ipv6 address %s", ipv4Address, ipv6Address)
+}
+
+func ipLink(exec executor.Executor) ([]InterfaceLink, error) {
+	links, err := exec.Exec("ip", "-j", "l")
+	if err != nil {
+		return nil, err
+	}
+	var interfaces []InterfaceLink
+	err = json.Unmarshal([]byte(links), &interfaces)
+	if err != nil {
+		return nil, err
+	}
+	return interfaces, nil
+}